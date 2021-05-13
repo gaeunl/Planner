3d70cc9e043df4da21efa55cbbd90df9dde0fa1b
Gaeun Lee <gaeun1065@gmail.com>
2021-05-12 23:47:28 -0700

    undo commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import {useEffect} from 'react';
+import { links } from './data';
+
+const Navbar = () => {
+    return(
+        <nav>
+            
+        </nav>
+    )
+
+}
+
+export default Navbar;
