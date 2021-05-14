01b0bce1f8e77610759fd208428469c79a8a6a8e
Gaeun Lee <gaeun1065@gmail.com>
2021-05-14 11:49:58 -0700

    close #7; hide or show menu on button click

--- src/Components/Nav/Links.js
+++ src/Components/Nav/Links.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {links} from './data';
 
-const Links = () => {
+const Links = (props) => {
     return(
-        <div className="nav-links show-container">
+        <div className={`${props.show?'nav-links show-container': 'nav-links'}`}>
             <ul className="links">
                 {links.map((link) => {
-                const{url, text, id} = link;
-                return(
-                    <li>
-                        <a href={url}>{text}</a>
-                    </li>
-                )
-            })}
+                    const{url, text, id} = link;
+                    return(
+                        <li key={id}>
+                            <a href={url}>{text}</a>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,18 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import logo from '../../Capture.svg';
 import Links from './Links';
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
     return(
         <nav className="nav-center">
             <div className="nav-header">
                 <img className="logo" src={logo} alt="planner logo"></img>
-                <button className="nav-toggle">
+                <button className="nav-toggle" onClick = {()=>setShowLinks(!showLinks)}>
                     <GiHamburgerMenu/>
                 </button>
             </div>
-            <Links/>
+            <Links show={showLinks}/>
+            {console.log(setShowLinks)}
             <ul className="menu-icon"></ul>
         </nav>
     )
